fdb83c0c8e06649e60e66620569cb0b21d5140df
dguka <dgukalin@gmail.com>
2025-06-03 11:49:40 +0300

    fix response handling vk

--- dialog_flow_api.py
+++ dialog_flow_api.py
@@ -4,13 +4,10 @@ from google.cloud import dialogflow
 
 def detect_intent_texts(project_id, session_id, text, language_code):
     try:
-
         session_client = dialogflow.SessionsClient()
-
         session = session_client.session_path(project_id, session_id)
 
         text_input = dialogflow.TextInput(text=text, language_code=language_code)
-
         query_input = dialogflow.QueryInput(text=text_input)
 
         response = session_client.detect_intent(
@@ -21,6 +18,23 @@ def detect_intent_texts(project_id, session_id, text, language_code):
         )
 
         return response.query_result.fulfillment_text
+
+    except (GoogleAPICallError, RetryError) as e:
+        return 'Я временно не могу отвечать, прошу прощения.'
+
+
+def detect_intent_response(project_id, session_id, text, language_code):
+    try:
+        session_client = dialogflow.SessionsClient()
+        session = session_client.session_path(project_id, session_id)
+
+        text_input = dialogflow.TextInput(text=text, language_code=language_code)
+        query_input = dialogflow.QueryInput(text=text_input)
+
+        response = session_client.detect_intent(
+            request={"session": session, "query_input": query_input}
+        )
+        return response
 
     except (GoogleAPICallError, RetryError) as e:
         return 'Я временно не могу отвечать, прошу прощения.'
--- vk_bot.py
+++ vk_bot.py
@@ -2,7 +2,7 @@ import vk_api
 from vk_api.exceptions import ApiError
 from vk_api.longpoll import VkEventType, VkLongPoll
 
-from dialog_flow_api import detect_intent_texts
+from dialog_flow_api import detect_intent_response
 
 
 def run_vk_bot(vk_token, project_id, language_code):
@@ -15,18 +15,21 @@ def run_vk_bot(vk_token, project_id, language_code):
 
         if event.type == VkEventType.MESSAGE_NEW:
             if event.to_me:
-                answer = detect_intent_texts(
+                response = detect_intent_response(
                     project_id,
                     event.user_id,
                     event.text,
                     language_code
                 )
 
-                try:
-                    vk.messages.send(
-                        user_id=event.user_id,
-                        message=answer,
-                        random_id=0
-                    )
-                except ApiError as api_err:
-                    continue
+                if not response.query_result.intent.is_fallback:
+                    answer = response.query_result.fulfillment_text
+
+                    try:
+                        vk.messages.send(
+                            user_id=event.user_id,
+                            message=answer,
+                            random_id=0
+                        )
+                    except ApiError:
+                        continue
